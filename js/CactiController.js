5378601b9b14bef92c2c664ac4f77358fec874bf
Vanderick Lee <130277439+vanderick-lee@users.noreply.github.com>
2023-07-28 19:19:33 +0700

    update cactus spawn rate max 2000 -> 1300

--- js/CactiController.js
+++ js/CactiController.js
@@ -2,7 +2,7 @@ import Cactus from "./Cactus.js";
 
 export default class CactiController {
   CACTUS_INTERVAL_MIN = 500;
-  CACTUS_INTERVAL_MAX = 2000;
+  CACTUS_INTERVAL_MAX = 1300;
 
   nextCactusInterval = null;
   cacti = [];
